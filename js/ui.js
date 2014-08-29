df1347631584106658d9a5cea04163aac371eb51
Linus Bohman <linus@linusbohman.se>
2014-08-29 17:48:31 +0200

    Got a basic grid working and resizing properly. Now to adjust .legible styles…

--- js/ui.js
+++ js/ui.js
@@ -5,7 +5,10 @@
   // Setup
   //
   window.leg = {
-    ref: {}
+    ref: {
+      css: {},
+      el: {}
+    }
   }
 
 
@@ -16,11 +19,18 @@
 
     // References
     references: function() {
-      leg.ref.css = '';
-      leg.ref.css_formatted = '';
-      leg.ref.css_array = {};
-      leg.ref.style_container = $('.legible-css');
-      leg.ref.controls = $('.controls');
+
+      // CSS
+      leg.ref.css.legible = '';
+      leg.ref.css.legible_formatted = '';
+      leg.ref.css.legible_array = {};
+      leg.ref.css.support = '';
+      leg.ref.css.support_array = '';
+
+      // Elements
+      leg.ref.el.legible_css = $('.legible-css');
+      leg.ref.el.support_css = $('.support-css');
+      leg.ref.el.controls = $('.controls');
     },
 
     // Build CSS at proper times
@@ -31,19 +41,19 @@
       leg.fn.build_css();
 
       // On change
-      leg.ref.controls.find('input').change(function(){
+      leg.ref.el.controls.find('input').change(function(){
         leg.fn.build_css();
         leg.fn.hash_update();
       });
 
       // On input
-      leg.ref.controls.find('input[type="range"]').on('input', function(){
+      leg.ref.el.controls.find('input[type="range"]').on('input', function(){
         leg.fn.build_css();
         leg.fn.hash_update();
       });
 
       // On keyup
-      leg.ref.controls.find('input[type="text"], input[type="number"]').keyup(function(){
+      leg.ref.el.controls.find('input[type="text"], input[type="number"]').keyup(function(){
         leg.fn.build_css();
         leg.fn.hash_update();
       });
@@ -51,7 +61,7 @@
 
     // Sync range and inputs
     sync: function() {
-      leg.ref.controls.find('input[type="range"]').each(function(){
+      leg.ref.el.controls.find('input[type="range"]').each(function(){
         var range = $(this);
         var input_id = range.attr('id').replace('-range', '-input');
         var input = $('#' + input_id);
@@ -91,14 +101,32 @@
   // 
   leg.fn = {
 
-    // Build CSS
+    // Show css or demo
+    toggle_css: function() {
+      $('html').toggleClass('showing-css');
+    },
+
+    // Build CSS: grab values entered in controls,
+    // do whatever calculations needed and update styles.
     build_css: function() {
-      leg.ref.css_array = [];
-      leg.ref.css = '';
-      leg.ref.css_formatted = '';
+
+      // Resetting
+      leg.ref.css.legible_array = [];
+      leg.ref.css.legible = '';
+      leg.ref.css.legible_formatted = '';
+      leg.ref.css.support_array = [];
+      leg.ref.css.support = '';
 
       // Spacing and sizing
-      leg.ref.css_array.push({
+      leg.ref.css.support_array.push({
+        selectors: ['.legible-test'],
+        styles: {
+          background_size: '100%' + ' ' + parseFloat($('#line-height-range').val() * 2) + 'em',
+          padding: $('#line-height-range').val() + 'em 40px'
+        }
+      });
+
+      leg.ref.css.legible_array.push({
         selectors: ['.legible'],
         styles: {
           font_size: $('#font-size-range').val() + 'px',
@@ -106,53 +134,83 @@
         }
       });
 
-      leg.ref.css_array.push({
+      leg.ref.css.legible_array.push({
         selectors: ['.legible h1', '.legible h2', '.legible h3', '.legible h4', '.legible h5', '.legible h6', '.legible div', '.legible p', '.legible ul', '.legible ol', '.legible blockquote', '.legible table', '.legible pre code'],
         styles: {
           margin_top: $('#block-spacing-range').val() + 'em'
         }
       });
 
-      leg.ref.css_array.push({
+      leg.ref.css.legible_array.push({
         selectors: ['.legible h1 + *', '.legible h2 + *', '.legible h3 + *', '.legible h4 + *', '.legible h5 + *', '.legible h6 + *', '.legible *:first-child'],
         styles: {
           margin_top: '0'
         }
       });
 
-      // Build css string
-      for (var i = 0; i < leg.ref.css_array.length; i++) {
+      // Lists
+      leg.ref.css.legible_array.push({
+        selectors: ['.legible ul', '.legible ol'],
+        styles: {
+          margin_left: $('#list-margin-range').val() + 'px',
+          padding_left: $('#list-padding-range').val() + 'px'
+        }
+      });
+
+      // Build css strings
+      for (var i = 0; i < leg.ref.css.legible_array.length; i++) {
+        var css_object = leg.ref.css.legible_array[i];
+        leg.ref.css.legible += leg.fn.build_css_from_object(css_object);
+      }
+
+      for (var i = 0; i < leg.ref.css.support_array.length; i++) {
+        var css_object = leg.ref.css.support_array[i];
+        leg.ref.css.support += leg.fn.build_css_from_object(css_object);
+      }
+
+      // Populate css
+      leg.ref.css.legible_formatted = leg.fn.format_css(leg.ref.css.legible);
+      leg.ref.el.legible_css.text(leg.ref.css.legible_formatted);
+      leg.ref.el.support_css.text(leg.ref.css.support);
+    },
+
+    // Give this function a CSS object and it'll return a css string.
+    // A CSS object look like this:
+    // {
+    //   selectors: [],
+    //   styles: { attribute: 'value' }
+    // }
+    // You need to switch - for _ in attributes, but otherwise it's all good.
+    build_css_from_object: function(css_object) {
+      var css_string = '';
+      if(css_object != undefined) {
 
         // Reference
-        var selectors = leg.ref.css_array[i].selectors;
-        var styles = leg.ref.css_array[i].styles;
+        var selectors = css_object.selectors;
+        var styles = css_object.styles;
 
         // Selectors
-        leg.ref.css += selectors.join(',');
+        css_string += selectors.join(',');
 
         // Starting bracket
-        leg.ref.css += '{';
+        css_string += '{';
 
         // Properties
         for (var attribute in styles) {
           if (styles.hasOwnProperty(attribute)) {
-            leg.ref.css += attribute.replace(/_/g, '-') + ':';
-            leg.ref.css += styles[attribute] + ';';
+            css_string += attribute.replace(/_/g, '-') + ':';
+            css_string += styles[attribute] + ';';
           }
         }
 
         // Ending bracket
-        leg.ref.css += '}';
+        css_string += '}';
       }
-
-      // Populate css
-      leg.ref.css_formatted = leg.fn.format_css(leg.ref.css);
-      leg.ref.style_container.text(leg.ref.css_formatted);
+      return css_string;
     },
 
     // Take ugly CSS string and make it look pretty.
-    // Based on code from: https://raw.githubusercontent.com/mrcoles/cssunminifier/master/lib/cssunminifier.js
-    // But modified to suit 040 CSS style
+    // Based on code from: https://raw.githubusercontent.com/mrcoles/cssunminifier/master/lib/cssunminifier.js but modified to suit 040 CSS style
     format_css: function(code, tab) {
       var defaultTab = 2;
       var space = '';
@@ -187,15 +245,10 @@
       return code;
     },
 
-    // Show css or demo
-    toggle_css: function() {
-      $('html').toggleClass('showing-css');
-    },
-
     // Update hash
     hash_update: function() {
       var hash_array = [];
-      leg.ref.controls.find('.field-group > input').each(function(index){
+      leg.ref.el.controls.find('.field-group > input').each(function(index){
         var value = $(this).val();
         hash_array.push(value);
       });
@@ -208,7 +261,7 @@
       var hash = window.location.hash.replace('#', '');
       if(hash.length > 1) {
         var hash_array = hash.split('_');
-        leg.ref.controls.find('.field-group > input').each(function(index){
+        leg.ref.el.controls.find('.field-group > input').each(function(index){
           $(this).val(hash_array[index]);
         });
       }
