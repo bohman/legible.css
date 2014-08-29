291e8a84c80dd9e3d08e0a531684badab2bc7fc2
Linus Bohman <linus@linusbohman.se>
2014-08-29 15:36:31 +0200

    Real basic value save in hash (probably need to redo sometime in the future, should really save the entire CSS string somehow)

--- js/ui.js
+++ js/ui.js
@@ -17,9 +17,36 @@
     // References
     references: function() {
       leg.ref.css = '';
+      leg.ref.css_formatted = '';
       leg.ref.css_array = {};
       leg.ref.style_container = $('.legible-css');
       leg.ref.controls = $('.controls');
+    },
+
+    // Build CSS at proper times
+    build_css: function() {
+
+      // On load
+      leg.fn.hash_set_values();
+      leg.fn.build_css();
+
+      // On change
+      leg.ref.controls.find('input').change(function(){
+        leg.fn.build_css();
+        leg.fn.hash_update();
+      });
+
+      // On input
+      leg.ref.controls.find('input[type="range"]').on('input', function(){
+        leg.fn.build_css();
+        leg.fn.hash_update();
+      });
+
+      // On keyup
+      leg.ref.controls.find('input[type="text"], input[type="number"]').keyup(function(){
+        leg.fn.build_css();
+        leg.fn.hash_update();
+      });
     },
 
     // Sync range and inputs
@@ -47,28 +74,7 @@
 
       });
     },
-
-    // Build CSS at proper times
-    build_runtimes: function() {
-
-      // On load
-      leg.fn.build_css();
-
-      // On change
-      leg.ref.controls.find('input').change(function(){
-        leg.fn.build_css();
-      });
-
-      // On input
-      leg.ref.controls.find('input[type="range"]').on('input', function(){
-        leg.fn.build_css();
-      });
-
-      // On keyup
-      leg.ref.controls.find('input[type="text"], input[type="number"]').keyup(function(){
-        leg.fn.build_css();
-      });
-    },
+    
 
     // Other interactions
     toggle_css: function() {
@@ -89,6 +95,7 @@
     build_css: function() {
       leg.ref.css_array = [];
       leg.ref.css = '';
+      leg.ref.css_formatted = '';
 
       // Spacing and sizing
       leg.ref.css_array.push({
@@ -113,43 +120,98 @@
         }
       });
 
-      // Format css string
+      // Build css string
       for (var i = 0; i < leg.ref.css_array.length; i++) {
 
         // Reference
         var selectors = leg.ref.css_array[i].selectors;
         var styles = leg.ref.css_array[i].styles;
 
-        // Spacing if needed
-        if(i > 0) {
-          leg.ref.css += '\n\n';
-        }
-
         // Selectors
-        leg.ref.css += selectors.join(',\n');
+        leg.ref.css += selectors.join(',');
 
         // Starting bracket
-        leg.ref.css += ' {';
+        leg.ref.css += '{';
 
         // Properties
         for (var attribute in styles) {
           if (styles.hasOwnProperty(attribute)) {
-            leg.ref.css += '\n  ';
-            leg.ref.css += attribute.replace(/_/g, '-') + ': ';
+            leg.ref.css += attribute.replace(/_/g, '-') + ':';
             leg.ref.css += styles[attribute] + ';';
           }
         }
 
         // Ending bracket
-        leg.ref.css += '\n}';
+        leg.ref.css += '}';
       }
 
-      leg.ref.style_container.text(leg.ref.css);
+      // Populate css
+      leg.ref.css_formatted = leg.fn.format_css(leg.ref.css);
+      leg.ref.style_container.text(leg.ref.css_formatted);
+    },
+
+    // Take ugly CSS string and make it look pretty.
+    // Based on code from: https://raw.githubusercontent.com/mrcoles/cssunminifier/master/lib/cssunminifier.js
+    // But modified to suit 040 CSS style
+    format_css: function(code, tab) {
+      var defaultTab = 2;
+      var space = '';
+
+      if (typeof tab == 'string') {
+        tab = /^\d+$/.test(tab) ? parseInt(tab) : defaultTab;
+      }
+
+      if (typeof tab == 'undefined') {
+        tab = defaultTab;
+      }
+
+      if (tab < 0) {
+        tab = defaultTab;
+      }
+
+      code = code
+        .split('\t').join('    ')
+        .replace(/\s*{\s*/g, ' {\n    ')
+        .replace(/;\s*/g, ';\n    ')
+        .replace(/,\s*/g, ',\n')
+        .replace(/[ ]*}\s*/g, '}\n')
+        .replace(/\}\s*(.+)/g, '}\n\n$1')
+        .replace(/\n    ([^:]+):\s*/g, '\n    $1: ')
+        .replace(/([A-z0-9\)])}/g, '$1;\n}');
+
+      if (tab != 4) {
+        for (;tab != 0;tab--) { space += ' '; }
+        code = code.replace(/\n    /g, '\n'+space);
+      }
+
+      return code;
     },
 
     // Show css or demo
     toggle_css: function() {
       $('html').toggleClass('showing-css');
+    },
+
+    // Update hash
+    hash_update: function() {
+      var hash_array = [];
+      leg.ref.controls.find('.field-group > input').each(function(index){
+        var value = $(this).val();
+        hash_array.push(value);
+      });
+      var hash = hash_array.join('_');
+      window.location.hash = hash;
+    },
+
+    // Set values from hash
+    hash_set_values: function() {
+      var hash = window.location.hash.replace('#', '');
+      if(hash.length > 1) {
+        var hash_array = hash.split('_');
+        leg.ref.controls.find('.field-group > input').each(function(index){
+          $(this).val(hash_array[index]);
+        });
+      }
     }
   }
 
