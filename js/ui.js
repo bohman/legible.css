d8f56b656f1444214ace04e1d284e6225d5e950d
Linus Bohman <linus@linusbohman.se>
2015-05-08 15:27:18 +0200

    Rebuilt output to get rid of grid. Didn’t work in our live projects.

--- js/ui.js
+++ js/ui.js
@@ -117,7 +117,7 @@
 
     // Toggle css
     toggle_grid: function() {
-      $('html').toggleClass('showing-grid');
+      $('html').toggleClass('display-helpers');
     },
 
     // Build CSS: grab values entered in controls,
@@ -132,49 +132,28 @@
       leg.ref.css.support = '';
 
       // Base value reference
-      var line_height = parseFloat($('#line-height-range').val());
       var font_size_px = parseFloat($('#font-size-range').val());
-      var line_height_px = Math.round(font_size_px * line_height);
-      var line_height_em = line_height_px / font_size_px;
+      var line_height_em = parseFloat($('#line-height-range').val());
+      var line_height_px = Math.round(font_size_px * line_height_em);
+      var block_spacing_em = parseFloat($('#block-spacing-range').val());
 
       var h_scale = parseFloat($('#h-scale-range').val());
-      var h_padding = parseFloat($('#h-padding-range').val());
-      var h_block_spacing = parseFloat($('#h-block-spacing').val());
+      var h_space_after = parseFloat($('#h-space-after-range').val());
 
-      var block_spacing = $('#block-spacing-range').val();
-      var block_spacing_px = block_spacing * line_height_px;
-      var block_spacing_em = parseFloat(block_spacing_px / font_size_px);
-      var block_spacing_after_header_em = parseFloat(h_block_spacing * block_spacing_em);
-
-      var h1_size = parseFloat($('#h1-size').val());
+      var h1_size_multiplier = parseFloat($('#h1-size-multiplier').val());
+      var h1_font_size_px = Math.round(Math.pow(h_scale, h1_size_multiplier) * font_size_px);
+      var h1_line_height_em = parseFloat($('#h1-line-height-range').val());
       var h1_font_weight = parseFloat($('#h1-font-weight').val());
-      var h1_font_size_px = Math.round(Math.pow(h_scale, h1_size) * font_size_px);
-      var h1_number_of_lines = Math.ceil(h1_font_size_px / line_height_px);
-      var h1_line_height_px = line_height_px * h1_number_of_lines;
-      var h1_font_size_em = parseFloat(h1_font_size_px/font_size_px);
-      var h1_line_height_em = parseFloat(h1_line_height_px/h1_font_size_px)
-      var h1_margin_top = parseFloat((line_height_px / h1_font_size_px) * block_spacing);
-      var h1_padding = parseFloat((line_height_px / h1_font_size_px) * h_padding);
 
-      var h2_size = parseFloat($('#h2-size').val());
+      var h2_size_multiplier = parseFloat($('#h2-size-multiplier').val());
+      var h2_font_size_px = Math.round(Math.pow(h_scale, h2_size_multiplier) * font_size_px);
+      var h2_line_height_em = parseFloat($('#h2-line-height-range').val());
       var h2_font_weight = parseFloat($('#h2-font-weight').val());
-      var h2_font_size_px = Math.round(Math.pow(h_scale, h2_size) * font_size_px);
-      var h2_number_of_lines = Math.ceil(h2_font_size_px / line_height_px);
-      var h2_line_height_px = line_height_px * h2_number_of_lines;
-      var h2_font_size_em = parseFloat(h2_font_size_px/font_size_px);
-      var h2_line_height_em = parseFloat(h2_line_height_px/h2_font_size_px);
-      var h2_margin_top = parseFloat((line_height_px / h2_font_size_px));
-      var h2_padding = parseFloat((line_height_px / h2_font_size_px) * h_padding);
 
-      var h3_size = parseFloat($('#h3-size').val());
+      var h3_size_multiplier = parseFloat($('#h3-size-multiplier').val());
+      var h3_font_size_px = Math.round(Math.pow(h_scale, h3_size_multiplier) * font_size_px);
+      var h3_line_height_em = parseFloat($('#h3-line-height-range').val());
       var h3_font_weight = parseFloat($('#h3-font-weight').val());
-      var h3_font_size_px = Math.round(Math.pow(h_scale, h3_size) * font_size_px);
-      var h3_number_of_lines = Math.ceil(h3_font_size_px / line_height_px);
-      var h3_line_height_px = line_height_px * h3_number_of_lines;
-      var h3_font_size_em = parseFloat(h3_font_size_px/font_size_px);
-      var h3_line_height_em = parseFloat(h3_line_height_px/h3_font_size_px);
-      var h3_margin_top = parseFloat((line_height_px / h3_font_size_px) * block_spacing);
-      var h3_padding = parseFloat((line_height_px / h3_font_size_px) * h_padding);
 
       var h4_font_weight = parseFloat($('#h4-font-weight').val());
 
@@ -184,22 +163,15 @@
       var list_ul_bullet_type = $('#list-ul-bullet-type').val();
       var list_ol_bullet_type = $('#list-ol-bullet-type').val();
 
-      var blockquote_size = parseFloat($('#blockquote-size').val());
-      var blockquote_font_size_px = Math.round(Math.pow(h_scale, blockquote_size) * font_size_px);
-      var blockquote_number_of_lines = Math.ceil(blockquote_font_size_px / line_height_px);
-      var blockquote_line_height_px = blockquote_number_of_lines * line_height_px;
-      
-      var blockquote_font_size_em = parseFloat(blockquote_font_size_px/font_size_px);
-      var blockquote_line_height_em = parseFloat(blockquote_line_height_px/blockquote_font_size_px);
-      var blockquote_one_line_em = line_height_px / blockquote_font_size_px;
-      var blockquote_margin_top = parseFloat(blockquote_one_line_em * block_spacing);
-      var blockquote_font_weight = $('#blockquote-font-weight').val();
+      var blockquote_size_multiplier = parseFloat($('#blockquote-size').val());
+      var blockquote_font_size_px = Math.round(Math.pow(h_scale, blockquote_size_multiplier) * font_size_px);
+      var blockquote_line_height_em = parseFloat($('#blockquote-line-height-range').val());
+      var blockquote_font_weight = parseFloat($('#blockquote-font-weight').val());
       var blockquote_font_style = $('#blockquote-font-style').val();
       var blockquote_background_color = $('#blockquote-background-color').val();
       var blockquote_color = $('#blockquote-color').val();
       var blockquote_margin_left_em = $('#blockquote-margin-left-range').val();
-      var blockquote_padding = $('#blockquote-padding-range').val();
-      var blockquote_padding_em = blockquote_one_line_em * blockquote_padding;
+      var blockquote_padding_em = $('#blockquote-padding-range').val();
 
       var b_font_weight = $('#b-font-weight').val();
       var code_background_color = $('#code-background-color').val();
@@ -209,14 +181,13 @@
       $('.line-height-px').text(line_height_px);
 
       // Support css, helps seeing things but shouldn't be added to legible
-      leg.ref.css.support_array.push({
-        selectors: ['.legible-test'],
-        styles: {
-          font_size: font_size_px + 'px',
-          line_height: line_height_em + 'em',
-          background_size: '100%' + ' ' + line_height_em * 2 + 'em'
-        }
-      });
+      //leg.ref.css.support_array.push({
+      //  selectors: ['.legible-test'],
+      //  styles: {
+      //    font_size: font_size_px + 'px',
+      //    line_height: line_height_em + 'em'
+      //  }
+      //});
 
       leg.ref.css.support_array.push({
         selectors: ['.legible-test .wrapper'],
@@ -252,57 +223,48 @@
       leg.ref.css.legible_array.push({
         selectors: ['.legible h1 + *', '.legible h2 + *', '.legible h3 + *', '.legible h4 + *', '.legible h5 + *', '.legible h6 + *'],
         styles: {
-          margin_top: block_spacing_after_header_em + 'em'
+          margin_top: h_space_after + 'em'
         }
       });
 
       // Headers
       leg.ref.css.legible_array.push({
         comment: 'Headers',
-        selectors: ['.legible h1'],
+        selectors: ['.header-1', '.legible .header-1', '.legible h1'],
         styles: {
-          font_size: h1_font_size_em + 'em',
+          font_size: h1_font_size_px + 'px',
           line_height: h1_line_height_em + 'em',
-          font_weight: h1_font_weight,
-          margin_top: h1_margin_top + 'em',
-          padding_top: h1_padding + 'em',
-          padding_bottom: h1_padding + 'em'
+          font_weight: h1_font_weight
         }
       });
 
       leg.ref.css.legible_array.push({
-        selectors: ['.legible h2'],
+        selectors: ['.header-2', '.legible .header-2', '.legible h2'],
         styles: {
-          font_size: h2_font_size_em + 'em',
+          font_size: h2_font_size_px + 'px',
           line_height: h2_line_height_em + 'em',
-          font_weight: h2_font_weight,
-          margin_top: h2_margin_top + 'em',
-          padding_top: h2_padding + 'em',
-          padding_bottom: h2_padding + 'em'
+          font_weight: h2_font_weight
         }
       });
 
       leg.ref.css.legible_array.push({
-        selectors: ['.legible h3'],
+        selectors: ['.header-3', '.legible .header-3', '.legible h3'],
         styles: {
-          font_size: h3_font_size_em + 'em',
+          font_size: h3_font_size_px + 'px',
           line_height: h3_line_height_em + 'em',
-          font_weight: h3_font_weight,
-          margin_top: h3_margin_top + 'em',
-          padding_top: h3_padding + 'em',
-          padding_bottom: h3_padding + 'em'
+          font_weight: h3_font_weight
         }
       });
 
       leg.ref.css.legible_array.push({
-        selectors: ['.legible h4'],
+        selectors: ['.header-4', '.legible .header-4', '.legible h4'],
         styles: {
           font_weight: h4_font_weight
         }
       });
 
       leg.ref.css.legible_array.push({
-        selectors: ['.legible h5'],
+        selectors: ['.header-5', '.legible .header-5', '.legible h5'],
         styles: {
           font_weight: h4_font_weight,
           font_style: 'italic'
@@ -310,7 +272,7 @@
       });
 
       leg.ref.css.legible_array.push({
-        selectors: ['.legible h6'],
+        selectors: ['.header-6', '.legible .header-6', '.legible h6'],
         styles: {
           font_style: 'italic'
         }
@@ -377,9 +339,8 @@
       leg.ref.css.legible_array.push({
         selectors: ['.legible blockquote'],
         styles: {
-          font_size: blockquote_font_size_em + 'em',
+          font_size: blockquote_font_size_px + 'px',
           line_height: blockquote_line_height_em + 'em',
-          margin_top: blockquote_margin_top + 'em',
           font_weight: blockquote_font_weight,
           font_style: blockquote_font_style,
           margin_left: blockquote_margin_left_em + 'em',
@@ -388,7 +349,6 @@
           background_color: blockquote_background_color
         }
       });
-
 
       // Build css strings
       for (var i = 0; i < leg.ref.css.legible_array.length; i++) {
